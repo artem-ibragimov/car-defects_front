c3bf66a69c465c4b3c568f0482f0082b797fd011
artem.ibragimov <artem.ibragimov@gmail.com>
2024-01-09 10:08:21 +0300

    Auto-commit

--- content/poster.js
+++ content/poster.js
@@ -4,24 +4,24 @@ import SocialPost from 'social-post-api';
 const social = new SocialPost('66H3ABB-NZ9MW1R-M6MWFSA-6VHPXRW');
 
 try {
-	const file = './content/posted.txt';
-	const posted = readFileSync(file).toString().split('\n');
-	const json = JSON.parse(readFileSync(`src/lib/i18n/en.json`).toString());
-	const poster = Object.keys(json.text.article).filter((t) => !posted.includes(t))[0];
-	const link = `https://car-defects.com/articles/en/${poster}/`;
-	const read_more = `...\n\nRead more: ${link}`;
-	post({
-		mediaUrls: [`https://car-defects.com/assets/img/${poster}.png`],
-		title: json.text.article[poster].title,
-		post: `${json.text.article[poster].text
-			.replaceAll('\n', '')
-			.slice(0, 280 - read_more.length - `[Sent with Free Plan] `.length)}${read_more}`,
-		link
-	})
-		.then(console.log, console.error)
-		.then(() => {
-			writeFileSync(file, posted.concat(poster).join('\n'));
-		});
+	// const file = './content/posted.txt';
+	// const posted = readFileSync(file).toString().split('\n');
+	// const json = JSON.parse(readFileSync(`src/lib/i18n/en.json`).toString());
+	// const poster = Object.keys(json.text.article).filter((t) => !posted.includes(t))[0];
+	// const link = `https://car-defects.com/articles/en/${poster}/`;
+	// const read_more = `...\n\nRead more: ${link}`;
+	// post({
+	// 	mediaUrls: [`https://car-defects.com/assets/img/${poster}.png`],
+	// 	title: json.text.article[poster].title,
+	// 	post: `${json.text.article[poster].text
+	// 		.replaceAll('\n', '')
+	// 		.slice(0, 280 - read_more.length - `[Sent with Free Plan] `.length)}${read_more}`,
+	// 	link
+	// })
+	// 	.then(console.log, console.error)
+	// 	.then(() => {
+	// 		writeFileSync(file, posted.concat(poster).join('\n'));
+	// 	});
 } catch (error) {
 	console.error(error);
 }
