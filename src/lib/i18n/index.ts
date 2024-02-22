822fcf605f73676dff819daf822cd70482275d69
artem.ibragimov <artem.ibragimov@gmail.com>
2024-02-22 18:08:55 +0300

    Auto-commit

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -8,17 +8,8 @@ import * as es from './es.json';
 // import * as pt from './pt.json';
 // import * as zh from './zh.json';
 
-export const DICTIONARIES = { en };
-// export const DICTIONARIES = { en, de, es, ru };
-// 	en: flatten<ILocale, ILocale>(en),
-// 	de: flatten<ILocale, ILocale>(de),
-// 	es: flatten<ILocale, ILocale>(es),
-// 	// fr: flatten<ILocale, ILocale>(fr),
-// 	// jp: flatten<ILocale, ILocale>(jp),
-// 	// pt: flatten<ILocale, ILocale>(pt),
-// 	ru: flatten<ILocale, ILocale>(ru)
-// 	// zh: flatten<ILocale, ILocale>(zh)
-// };
+// export const DICTIONARIES = { en };
+export const DICTIONARIES = { en, de, es, ru };
 
 export const ARTICLES = Object.keys(en.text.article);
 export const AVAILABLE_LOCALES = Object.keys(DICTIONARIES);
