8406e2144a3f086d7cfe88e8ac310e63f10d6cc1
artem.ibragimov <artem.ibragimov@gmail.com>
2024-07-19 11:35:19 +0300

    Auto-commit

--- content/poster.js
+++ content/poster.js
@@ -6,14 +6,15 @@ const social = new SocialPost('66H3ABB-NZ9MW1R-M6MWFSA-6VHPXRW');
 try {
 	const file = './content/posted.txt';
 	const posted = readFileSync(file).toString().split('\n');
-	const json = JSON.parse(readFileSync(`src/lib/i18n/en.json`).toString());
+	const json = JSON.parse(readFileSync(`src/lib/i18n/article_en.json`).toString());
 	const poster = Object.keys(json.text.article).filter((t) => !posted.includes(t))[0];
 	const link = `https://car-defects.com/articles/en/${poster}/`;
 	const read_more = `...\n\nRead more: ${link}`;
 	post({
-		mediaUrls: [], // [`https://car-defects.com/assets/img/${poster}.png`],
+		mediaUrls: [`https://car-defects.com/assets/img/${poster}.png`],
+		keywords: json.text.article[poster].tikeywordstle,
 		title: json.text.article[poster].title,
-		post: `${json.text.article[poster].text
+		post: `${json.text.article[poster].description
 			.replaceAll('\n', '')
 			.slice(0, 280 - read_more.length - `[Sent with Free Plan] `.length)}${read_more}`,
 		link
@@ -26,9 +27,10 @@ try {
 	console.error(error);
 }
 
-function post({ title, post, link, mediaUrls }) {
+function post({ title, post, keywords, link, mediaUrls }) {
 	const cfg = {
 		post,
+		keywords,
 		shortenLinks: false,
 		max: 3, // optional, range 1-5
 		position: 'auto' // optional, "auto" or "end"
