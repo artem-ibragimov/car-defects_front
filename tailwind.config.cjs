e61d888a48f88bc839f7e021b60656274c79f432
artem.ibragimov <artem.ibragimov@gmail.com>
2024-11-07 09:53:29 +0300

    Auto-commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,7 @@ module.exports = {
 	content: ['./src/**/*.{svelte,js,ts}'],
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
-		themes: ['pastel', 'dim'],
-		darkTheme: 'dim',
+		themes: ['pastel'],
 		base: true, // applies background color and foreground color for root element by default
 		styled: true, // include daisyUI colors and design decisions for all components
 		utils: true, // adds responsive and modifier utility classes
