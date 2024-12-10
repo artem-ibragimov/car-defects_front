9d169a2eaed5d5e5f77411d948ab3e78345f3dad
artem.ibragimov <artem.ibragimov@gmail.com>
2024-12-10 12:28:23 +0400

    Auto-commit

--- content/generator/Article.ts
+++ content/generator/Article.ts
@@ -119,7 +119,7 @@ export class Article {
 	}
 
 	get poster() {
-		return `${this.cars.join(' and ')} as an article poster, photo realistic, without text, fullscreen, no paddings –ar 2:1 `;
+		return `${this.cars.join(' and ')} as an article poster, comics style, without text, fullscreen, no paddings –ar 2:1 `;
 	}
 
 	private get title() {
